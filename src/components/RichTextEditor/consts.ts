68a8c49c3cc840952b25b0755efc4663e86d3c1f
mhcassiem <mhcassiem@gmail.com>
2024-04-21 02:38:39 +0200

    overide image setter in imagetool

--- src/components/RichTextEditor/consts.ts
+++ src/components/RichTextEditor/consts.ts
@@ -3,10 +3,28 @@ import strikethroughIcon from "@dashboard/icons/StrikethroughIcon";
 import { ToolConstructable, ToolSettings } from "@editorjs/editorjs";
 import Embed from "@editorjs/embed";
 import Header from "@editorjs/header";
+import ImageTool from "@editorjs/image";
 import List from "@editorjs/list";
 import Paragraph from "@editorjs/paragraph";
 import Quote from "@editorjs/quote";
 import createGenericInlineTool from "editorjs-inline-tool";
+
+export class CustomImageTool extends ImageTool {
+  // eslint-disable-next-line accessor-pairs
+  set image(file) {
+    // @ts-expect-error
+    this._data.file = file || {};
+
+    if (file && file.url) {
+      // @ts-expect-error
+      this.ui.fillImage(file.url);
+      // @ts-expect-error
+      this.ui.nodes.imageContainer.querySelector(
+        ".image-tool__image-picture",
+      ).id = this._data.file.id;
+    }
+  }
+}
 
 const inlineToolbar = ["link", "bold", "italic", "strikethrough"];
 
